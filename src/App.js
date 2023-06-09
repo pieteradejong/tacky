c3e4f3969c5f08558ffc93b2120fa2701cee1357
Pieter de Jong <pieter.a.dejong@gmail.com>
2023-06-09 15:30:42 -0400

    create board, add square component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,36 @@
-import React from "react";
+import React, { Fragment } from "react";
+import { useState } from "react";
 
-export default function Square() {
-  return <button className="square">X</button>;
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue('X');
+  }
+  return <button onClick={handleClick} className="square">{value}</button>;  
+}
+
+
+export default function Board() {
+  return (
+    <Fragment>
+      <div className="board-row">
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
+      </div>
+      
+      <div className="board-row">
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
+      </div>
+      
+      <div className="board-row">
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
+      </div>
+    </Fragment>
+    );
 }
